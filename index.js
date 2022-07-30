182a978db0bcfb2fb29a2898a2e845835138608e
KawserAlam21 <mdnaiyem.islam.10@gmail.com>
2022-07-30 22:00:11 +0600

    protect some routes

--- index.js
+++ index.js
@@ -1,78 +1,107 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const { MongoClient } = require("mongodb");
-const port = process.env.PORT ||5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-const user = process.env.DB_USER
-const pass = process.env.DB_PASS
+
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
 
 const uri = `mongodb+srv://${user}:${pass}@cluster0.6w64q.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
-async function run() {
-    try {
-        await client.connect();
-        const database = client.db("doctors_portal");
-        const appointmentCollection = database.collection("appointment");
-        const usersCollection = database.collection("users");
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+    // console.log(decoded.foo)
+  });
+}
 
-     //load data from database
-     app.get("/appointments", async (req, res) => {
-        const email = req.query.email;
-        const date = req.query.date;
-        const query = { email:email, date:date };
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("doctors_portal");
+    const appointmentCollection = database.collection("appointment");
+    const usersCollection = database.collection("users");
+
+    //load data from database
+    app.get("/appointments", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const date = req.query.date;
+      const decodedEmail = req.decoded.email;
+      if (email === decodedEmail) {
+        const query = { email: email, date: date };
         const cursor = appointmentCollection.find(query);
         const appointments = await cursor.toArray();
-        res.json(appointments);
-     });
+        return res.json(appointments);
+      } else {
+        return res.status(401).send("Forbidden Access");
+      }
+    });
 
-     //sending appointment data to database
-        app.post("/appointments", async (req, res) => {
-            const appointmentData = req.body
-            const result = await appointmentCollection.insertOne(appointmentData);
-            res.json(result);
-        });
-     
-     //sending users data to users collection
-        app.post("/users", async (req, res) => {
-            const userData = req.body;
-            const result = await usersCollection.insertOne(userData);
-            res.json(result);
-        });
+    app.get("/users", verifyJWT, async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    });
 
-        app.put("/users", async (req, res) => {
-            const userData = req.body;
-            const filter = { email: userData.email};
-            console.log(filter);
-            const options = { upsert: true };
-            const updateDoc = { $set: userData };
-            const result = await usersCollection.updateOne(filter, updateDoc, options);
-            res.json(result);
-        });
+    //sending appointment data to database
+    app.post("/appointments", async (req, res) => {
+      const appointmentData = req.body;
+      const result = await appointmentCollection.insertOne(appointmentData);
+      res.json(result);
+    });
 
+    //sending users data to users collection
+    app.post("/users", async (req, res) => {
+      const userData = req.body;
+      const result = await usersCollection.insertOne(userData);
+      res.json(result);
+    });
 
+    app.put("/users", async (req, res) => {
+      const userData = req.body;
+      const filter = { email: userData.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: userData };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: userData.email },
+        process.env.ACCESS_SECRET_TOKEN,
+        { expiresIn: "1hr" }
+      );
+      res.json({ result, token });
+    });
 
-        console.log('Database Connected Successfully');
-      
-    } finally {
+    console.log("Database Connected Successfully");
+  } finally {
     //   await client.close();
-    }
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send("Doctors Portal Server Is Running");
+app.get("/", (req, res) => {
+  res.send("Doctors Portal Server Is Running");
 });
 
-
-
-app.listen(port, () =>{
-    console.log("Listening on port", port);
+app.listen(port, () => {
+  console.log("Listening on port", port);
 });
